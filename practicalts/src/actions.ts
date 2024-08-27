1bd80b74aab5ffeffe90f752b4637a4f3c8cb185
syrr0143 <sy816120@gmail.com>
2024-08-27 23:52:55 +0530

    updated

---
+++ practicalts/src/actions.ts
@@ -0,0 +1,11 @@
+const sumitfunction = (value: string): string => {
+    return value;
+}
+
+const returnname = (name: string, age: number) => {
+    return {
+        name, age
+    }
+}
+
+export { sumitfunction, returnname }
